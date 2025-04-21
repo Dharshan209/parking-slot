16704769bb94dc6cd5730e6c04d1b8b1e96a5af0
Dharshan senthil <131781900+Dharshan209@users.noreply.github.com>
2025-04-21 17:59:02 +0530

    creating services

--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,BadRequestException} from '@nestjs/common';
+import {car} from "./interface/car.interface";
 
 @Injectable()
-export class ParkingService {}
+export class ParkingService {
+    private totalslots : number =0;
+    private avalibleslots : number[] = [];
+    
+    //Fisrt set parking lot size
+    initialize(no_of_slot:number){
+       this.totalslots=no_of_slot;
+       this.avalibleslots=Array.from({length:no_of_slot},(_,i)=>i+1);
+       return {total_slot : this.totalslots};
+    }
+
+    //this expand already parking slots instansexc
+    expand(increment:number){
+        const n = this.totalslots;
+        const additionalslot = Array.from({length:increment},(_,i)=>n+i+1);
+        this.avalibleslots.push(...additionalslot);
+        return{
+            total_slots:this.totalslots
+        };
+    }
+
+    //allcate car to slot
+    Park(Car:car){
+        if(this.avalibleslots.length==0){
+            throw new BadRequestException('Parking is full.speed up dude');
+        }
+
+        
+    }
+}
